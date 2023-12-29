12cc0d1eec5709a0ca2cf4364ea163120dcaf7b9
svet-nikol <plaksina.sn@mail.ru>
2023-12-29 14:10:10 +0300

    добавлены hooks, рендер списков через ключи, ф-л addCard

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,42 @@ import MainContent from "./components/MainContent/MainContent";
 import PopBrowse from "./components/pop-up/PopBrowse/PopBrowse";
 import PopNewCard from "./components/pop-up/PopNewCard/PopNewCard";
 import PopExit from "./components/pop-up/PopExit/PopExit";
+import { useEffect, useState } from "react";
+import { cardList } from "./data";
 
 function App() {
+  const [cards, setCards] = useState(cardList);
+
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false);
+    }, 1000);
+  }, []);
+
+  function addCard() {
+    setCards([
+      ...cards,
+      {
+        id: cards.length + 1,
+        theme: "Copywriting",
+        title: "Новая задача",
+        date: "30.10.23",
+        status: "Без статуса",
+        classNameTheme: "_purple",
+      },
+    ]);
+  }
+
   return (
     <Wrapper>
       <PopExit />
       <PopNewCard />
       <PopBrowse />
 
-      <Header />
-      <MainContent />
+      <Header addCard={addCard} />
+      <MainContent isLoaded={isLoaded} cardList={cards} />
     </Wrapper>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,10 @@
-function Header() {
+import { useState } from "react";
+
+function Header({addCard}) {
+  const [isOpened, setIsOpened] = useState(false);
+  function togglePopUp() {
+    setIsOpened((prev) => !prev)
+  }
   return (
     <header className="header">
       <div className="container">
@@ -14,27 +20,28 @@ function Header() {
             </a>
           </div>
           <nav className="header__nav">
-            <button className="header__btn-main-new _hover01" id="btnMainNew">
-              <a href="#popNewCard">Создать новую задачу</a>
+            <button className="header__btn-main-new _hover01" id="btnMainNew" onClick={addCard}>
+            Создать новую задачу
             </button>
-            <a href="#user-set-target" className="header__user _hover02">
+            <a href="#" className="header__user _hover02" onClick={togglePopUp}>
               Ivan Ivanov
             </a>
-            <div
-              className="header__pop-user-set pop-user-set"
-              id="user-set-target"
-            >
-              {/* <a href="">x</a> */}
-              <p className="pop-user-set__name">Ivan Ivanov</p>
-              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-              <div className="pop-user-set__theme">
-                <p>Темная тема</p>
-                <input type="checkbox" className="checkbox" name="checkbox" />
-              </div>
-              <button type="button" className="_hover03">
-                <a href="#popExit">Выйти</a>
-              </button>
-            </div>
+            {isOpened && 
+                        <div
+                        className="header__pop-user-set pop-user-set"
+                      >
+                        {/* <a href="">x</a> */}
+                        <p className="pop-user-set__name">Ivan Ivanov</p>
+                        <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                        <div className="pop-user-set__theme">
+                          <p>Темная тема</p>
+                          <input type="checkbox" className="checkbox" name="checkbox" />
+                        </div>
+                        <button type="button" className="_hover03">
+                          <a href="#popExit">Выйти</a>
+                        </button>
+                      </div>
+            }
           </nav>
         </div>
       </div>
