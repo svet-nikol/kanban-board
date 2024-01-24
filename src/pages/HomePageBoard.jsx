51c4f1f22a213bed1cda33d5e79563d52920ccaa
svet-nikol <plaksina.sn@mail.ru>
2024-01-24 15:20:05 +0300

    errors getting tasks from the API

--- src/pages/HomePageBoard.jsx
+++ src/pages/HomePageBoard.jsx
@@ -11,13 +11,19 @@ import { getCards } from "../lib/api.js";
 export default function HomePageBoard() {
   const [cards, setCards] = useState(cardList);
   const [getCardsIsLoaded, setGetCardsIsLoaded] = useState(false);
+  const [getCardsError, setGetCardsError] = useState(null);
   useEffect(() => {
     setGetCardsIsLoaded(true);
-    getCards().then((cards) => {
-      console.log(cards);
-      setGetCardsIsLoaded(false);
-      setCards(cards.tasks);
-    });
+    getCards()
+      .then((cards) => {
+        setCards(cards.tasks);
+      })
+      .catch((error) => {
+        setGetCardsError(error.message);
+      })
+      .finally(() => {
+        setGetCardsIsLoaded(false);      
+      });
   }, []);
 
   return (
@@ -27,7 +33,11 @@ export default function HomePageBoard() {
         <Outlet />
 
         <Header />
-        <MainContent isLoaded={getCardsIsLoaded} cardList={cards} />
+        {getCardsError ? (
+          <p style={{ color: "red" }}>{getCardsError}</p>
+        ) : (
+          <MainContent isLoaded={getCardsIsLoaded} cardList={cards} />
+        )}
       </Wrapper>
     </>
   );
