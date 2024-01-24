7c2d30062abaef49ba8566812bdf143791d414e2
svet-nikol <plaksina.sn@mail.ru>
2024-01-24 14:52:30 +0300

    get tasks from the API

--- src/pages/HomePageBoard.jsx
+++ src/pages/HomePageBoard.jsx
@@ -6,34 +6,29 @@ import Header from "../components/Header/Header.jsx";
 import MainContent from "../components/MainContent/MainContent.jsx";
 import "../App.css";
 import { Outlet } from "react-router-dom";
-
+import { getCards } from "../lib/api.js";
 
 export default function HomePageBoard() {
+  const [cards, setCards] = useState(cardList);
+  const [getCardsIsLoaded, setGetCardsIsLoaded] = useState(false);
+  useEffect(() => {
+    setGetCardsIsLoaded(true);
+    getCards().then((cards) => {
+      console.log(cards);
+      setGetCardsIsLoaded(false);
+      setCards(cards.tasks);
+    });
+  }, []);
 
-    const [isLoaded, setIsLoaded] = useState(true);
-  
-    useEffect(() => {
-      setTimeout(() => {
-        setIsLoaded(false);
-      }, 300);
-    }, []);
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Outlet />
 
-    const [cards, setCards] = useState(cardList);
-    useEffect(() => {
-      setCards(cardList);
-    }, [cards]);
-
-    return (
-      <>
-        <GlobalStyle />
-        <Wrapper>
-
-          <Outlet />
-  
-          <Header />
-          <MainContent isLoaded={isLoaded} cardList={cards} />
-
-        </Wrapper>
-      </>
-    );
+        <Header />
+        <MainContent isLoaded={getCardsIsLoaded} cardList={cards} />
+      </Wrapper>
+    </>
+  );
 }
